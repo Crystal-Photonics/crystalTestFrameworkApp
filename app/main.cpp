555242c0d5a162b71ded20cd27598aac3dec1715
Tobias Rieger <tr@crystal-photonics.com>
2017-01-10 16:19:09 +0100

    Added assert for Qt error messages and warnings to have a stack trace available to figure out where the error happened.

--- app/main.cpp
+++ app/main.cpp
@@ -2,12 +2,27 @@
 
 #include <QApplication>
 #include <QDebug>
+#include <cassert>
+
+static QtMessageHandler old_handler;
+
+void message_handler(QtMsgType type, const QMessageLogContext &context, const QString &msg) {
+	switch (type) {
+		case QtWarningMsg:
+		case QtCriticalMsg:
+		case QtFatalMsg:
+			assert(false);
+		case QtDebugMsg:
+		case QtInfoMsg:;
+	}
+	old_handler(type, context, msg);
+}
 
 int main(int argc, char *argv[]) {
 	QCoreApplication::setOrganizationName("CPG");
 	QCoreApplication::setApplicationName("Crystal Test Framework App");
-
-    QApplication a(argc, argv);
+	old_handler = qInstallMessageHandler(message_handler);
+	QApplication a(argc, argv);
     MainWindow w;
     w.show();
     return a.exec();
