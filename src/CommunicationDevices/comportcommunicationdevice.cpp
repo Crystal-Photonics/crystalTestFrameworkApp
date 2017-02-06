d6541574a0e479573d2d8c1ed3186aca9a8a5998
Tobias Rieger <tr@crystal-photonics.com>
2017-02-06 15:56:54 +0100

    Limiting the scope of reading from comport to just the reading.

--- src/CommunicationDevices/comportcommunicationdevice.cpp
+++ src/CommunicationDevices/comportcommunicationdevice.cpp
@@ -21,26 +21,25 @@ bool ComportCommunicationDevice::connect(const QSerialPortInfo &portinfo, QSeria
 }
 
 bool ComportCommunicationDevice::waitReceived(Duration timeout, int bytes) {
-	return Utility::promised_thread_call(this, [this, timeout, bytes] {
-		auto now = std::chrono::high_resolution_clock::now();
-		int received_bytes = 0;
-		auto try_read = [this, &received_bytes] {
+	auto now = std::chrono::high_resolution_clock::now();
+	int received_bytes = 0;
+	auto try_read = [this, &received_bytes] {
+		auto result = Utility::promised_thread_call(this, [this] {
 			QApplication::processEvents();
-			auto result = port.readAll();
-			if (result.isEmpty() == false) {
-				emit received(result);
-				received_bytes += result.size();
-			}
-		};
-		currently_in_waitReceived = true;
-		do {
-			try_read();
-		} while (received_bytes < bytes && std::chrono::high_resolution_clock::now() - now < timeout);
+			return port.readAll();
+		});
+		if (result.isEmpty() == false) {
+			emit received(result);
+			received_bytes += result.size();
+		}
+	};
+	currently_in_waitReceived = true;
+	do {
 		try_read();
-		QApplication::processEvents();
-		currently_in_waitReceived = false;
-		return received_bytes >= bytes;
-	});
+	} while (received_bytes < bytes && std::chrono::high_resolution_clock::now() - now < timeout);
+	try_read();
+	currently_in_waitReceived = false;
+	return received_bytes >= bytes;
 }
 
 void ComportCommunicationDevice::send(const QByteArray &data, const QByteArray &displayed_data) {
