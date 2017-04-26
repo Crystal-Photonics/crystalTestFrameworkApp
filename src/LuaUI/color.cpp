c118a05bb95db3d63bfec4545850799b4154e103
Tobias Rieger <tr@crystal-photonics.com>
2017-04-26 16:28:00 +0200

    Changed Color class to reflect adding overloaded_function and generate correct doxygen.

--- src/LuaUI/color.cpp
+++ src/LuaUI/color.cpp
@@ -1,21 +1,11 @@
 #include "color.h"
 #include <QColor>
 
-///\cond HIDDEN_SYMBOLS
 Color::Color(int rgb)
     : rgb(rgb) {}
-///\endcond
 
-Color Color::Color_from_name(const std::string &name) {
-    int c = QColor{name.c_str()}.rgb();
-    return Color{c};
-}
+Color::Color(const std::string &name)
+	: rgb(QColor{name.c_str()}.rgb()) {}
 
-Color Color::Color_from_r_g_b(int r, int g, int b) {
-    int c = (r & 0xFF) << 16 | (g & 0xFF) << 8 | (b & 0xFF);
-    return Color{c};
-}
-
-Color Color::Color_from_rgb(int rgb) {
-    return Color{rgb};
-}
+Color::Color(int r, int g, int b)
+	: rgb{(r & 0xFF) << 16 | (g & 0xFF) << 8 | (b & 0xFF)} {}
