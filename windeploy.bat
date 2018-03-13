cb63aa4923abc57d8ac5e26727acad3cfee44a06
tr <tr@crystal-photonics.com>
2018-03-13 16:51:15 +0100

    Changed qwt lib path to internal library. Also added error code printing and returning.

--- windeploy.bat
+++ windeploy.bat
@@ -1,17 +1,22 @@
-PATH_old=%PATH%
-PATH=%PATH%;C:\Qt\Tools\mingw530_32\bin
+SET PATH=%PATH%;C:\Qt\Tools\mingw530_32\bin
 
 echo %PATH%
 
 set CTFW_RELEASE_PATH=..\build-crystalTestFrameworkApp-Desktop_Qt_5_8_0_MinGW_32bit-Release\bin\release\
-set QWT_PATH=C:\Qt\build-qwt-Desktop_Qt_5_8_0_MinGW_32bit-Release\lib\
+set QWT_PATH=libs\qwt
 
-copy libs\LimeReport\build\5.8.0\win32\release\lib\limereport.dll %CTFW_RELEASE_PATH%
-copy libs\LimeReport\build\5.8.0\win32\release\lib\QtZint.dll %CTFW_RELEASE_PATH%
+copy libs\LimeReport\build\5.8.0\win32\release\lib\limereport.dll %CTFW_RELEASE_PATH% || goto :FAIL
+copy libs\LimeReport\build\5.8.0\win32\release\lib\QtZint.dll %CTFW_RELEASE_PATH% || goto :FAIL
 
-copy %QWT_PATH%qwt.dll %CTFW_RELEASE_PATH%
+copy %QWT_PATH%\lib\qwt.dll %CTFW_RELEASE_PATH% || GOTO :FAIL
 
-C:\Qt\5.8\mingw53_32\bin\windeployqt -network -serialport -sql -opengl -printsupport -script -xml -test %CTFW_RELEASE_PATH%crystalTestFramework.exe 
+C:\Qt\5.8\mingw53_32\bin\windeployqt -network -serialport -sql -opengl -printsupport -script -xml -test %CTFW_RELEASE_PATH%crystalTestFramework.exe  || GOTO :FAIL
 
-set PATH=%PATH_old%
+@goto :EOF
+:FAIL
+@echo off
+IF %ERRORLEVEL% NEQ 0 (
+	echo Error %ERRORLEVEL%, aborting
+	EXIT /B %ERRORLEVEL%
+)
 
