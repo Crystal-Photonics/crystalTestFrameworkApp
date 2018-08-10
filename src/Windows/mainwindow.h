4fdf402be62c850fe83f98796b0f051a56c7b143
Tobias Rieger <tr@crystal-photonics.com>
2018-08-10 15:36:16 +0200

    -fixes #1087
    Fixed critical crash bug.

--- src/Windows/mainwindow.h
+++ src/Windows/mainwindow.h
@@ -42,7 +42,7 @@ class EXPORT MainWindow : public QMainWindow {
     static QThread *gui_thread;
 
     template <class Function>
-    void execute_in_gui_thread(Function &&f);
+	void execute_in_gui_thread(Function f);
 
     template <class Lua_UI_class, class... Args>
     void add_lua_UI_class(int id, UI_container *parent, Args &&... args);
@@ -124,8 +124,8 @@ bool operator==(const T *lhs, const std::unique_ptr<T> &rhs) {
 }
 
 template <class Function>
-void MainWindow::execute_in_gui_thread(Function &&f) {
-    Utility::thread_call(this, nullptr, std::forward<Function>(f));
+void MainWindow::execute_in_gui_thread(Function f) {
+	Utility::thread_call(this, nullptr, std::move(f));
 }
 
 template <class Lua_UI_class>
