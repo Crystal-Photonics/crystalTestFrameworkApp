f2ed30733b02f08fb9d8487f388222545d5ff80f
arne krüger <ak@crystal-photonics.com>
2019-04-24 11:42:16 +0200

    -corrects git branch display in info window

--- src/Windows/infowindow.cpp
+++ src/Windows/infowindow.cpp
@@ -7,7 +7,8 @@ InfoWindow::InfoWindow(QWidget *parent)
     ui->setupUi(this);
     ui->lbl_git_date->setText("Git Date: " + QString(GITDATE));
     ui->lbl_git_hash->setText("Git Hash: 0x" + QString::number(GITHASH, 16));
-    ui->lbl_build_branch->setText("Git Branch: " + QString(GITBRANCH).split(" ").last());
+    auto git_branch = QString(GITBRANCH).split("->").last().split(",")[0]; // "HEAD -> master, origin/master, origin/dev, origin/HEAD"
+    ui->lbl_build_branch->setText("Git Branch: " + git_branch);
 
     ui->lbl_build_date->setText(QString("Build date: ") + QString(__DATE__) + " " + QString(__TIME__));
 }
