3cefdc13d1559a451a0574aded556068231782ec
Tobias Rieger <tr@crystal-photonics.com>
2020-01-08 14:54:05 +0100

    -fixes #1307
    No longer blocks until a device can be opened which can take 20 seconds
    in practice during which no other devices are serviced which can cause
    timeout issues. Also indicating device status via colors.

--- src/Protocols/sg04countprotocol.cpp
+++ src/Protocols/sg04countprotocol.cpp
@@ -22,7 +22,7 @@ static uint16_t parse_sg04_count_package(uint8_t indata[4], bool &ok) {
 SG04CountProtocol::SG04CountProtocol(CommunicationDevice &device, DeviceProtocolSetting setting)
     : Protocol{"SG04Count"}
     , device(&device)
-	, device_protocol_setting(std::move(setting)) {
+    , device_protocol_setting(std::move(setting)) {
 #if 1
     connection = QObject::connect(&device, &CommunicationDevice::received, [&device, this](const QByteArray &data) {
         incoming_data.append(data);
@@ -45,17 +45,17 @@ SG04CountProtocol::SG04CountProtocol(CommunicationDevice &device, DeviceProtocol
                         actual_count_rate = counts;
 
                         received_counts += counts;
-						{
-							std::unique_lock received_lock{received_counts_mutex};
-							received_count_packages.append(counts);
-							last_package = std::chrono::system_clock::now();
-							int max_count_entries = 1;
-							if (device.is_in_use()) {
-								max_count_entries = 1000;
-							}
-							if (received_count_packages.count() > max_count_entries) {
-								received_count_packages.removeFirst();
-							}
+                        {
+                            std::unique_lock received_lock{received_counts_mutex};
+                            received_count_packages.append(counts);
+                            last_package = std::chrono::system_clock::now();
+                            int max_count_entries = 1;
+                            if (device.is_in_use()) {
+                                max_count_entries = 1000;
+                            }
+                            if (received_count_packages.count() > max_count_entries) {
+                                received_count_packages.removeFirst();
+                            }
                         }
                         right_offset_found = true;
                         //qDebug() << "SG04-Count received" << counts;
@@ -74,9 +74,10 @@ SG04CountProtocol::SG04CountProtocol(CommunicationDevice &device, DeviceProtocol
 }
 
 SG04CountProtocol::~SG04CountProtocol() {
-    assert(connection);
-    auto result = QObject::disconnect(connection);
-    assert(result);
+    if (connection) {
+        auto result = QObject::disconnect(connection);
+        assert(result);
+    }
 }
 
 void SG04CountProtocol::set_ui_description(QTreeWidgetItem *ui_entry) {
@@ -91,24 +92,24 @@ void SG04CountProtocol::sg04_counts_clear_raw() {
 }
 
 void SG04CountProtocol::sg04_counts_clear() {
-	std::unique_lock received_lock{received_counts_mutex};
-	sg04_counts_clear_raw();
+    std::unique_lock received_lock{received_counts_mutex};
+    sg04_counts_clear_raw();
 }
 
 sol::table SG04CountProtocol::get_sg04_counts(sol::state &lua, bool clear) {
     sol::table result = lua.create_table_with();
     sol::table counts_table = lua.create_table_with();
-	{
-		std::unique_lock received_lock{received_counts_mutex};
-		for (auto i : received_count_packages) {
-			counts_table.add(i);
-		}
+    {
+        std::unique_lock received_lock{received_counts_mutex};
+        for (auto i : received_count_packages) {
+            counts_table.add(i);
+        }
+        if (clear) {
+            sg04_counts_clear_raw();
+        }
     }
-	result["total"] = received_counts;
+    result["total"] = received_counts;
     result["counts"] = counts_table;
-    if (clear) {
-        sg04_counts_clear();
-    }
     return result;
 }
 
@@ -125,21 +126,21 @@ uint SG04CountProtocol::accumulate_counts(ScriptEngine *script_engine, uint time
     int timeout_interval = time_ms / SG04_COUNT_INTERVAL_MS;
 
     while (timeout_interval > 0) {
-		if (not device->isConnected()) {
-			throw std::runtime_error{"SG04 Device Error: Tried waiting on closed device"};
-		}
+        if (not device->isConnected()) {
+            throw std::runtime_error{"SG04 Device Error: Tried waiting on closed device"};
+        }
         script_engine->await_timeout(std::chrono::milliseconds{SG04_COUNT_INTERVAL_MS} * 2);
-		std::unique_lock received_lock{received_counts_mutex};
-		for (auto i : received_count_packages) {
-			result += i;
-			timeout_interval--;
-			if (timeout_interval == 0) {
-				break;
+        std::unique_lock received_lock{received_counts_mutex};
+        for (auto i : received_count_packages) {
+            result += i;
+            timeout_interval--;
+            if (timeout_interval == 0) {
+                break;
             }
-		}
-		sg04_counts_clear_raw();
-	}
-	//qDebug() << "elapsed time: " << QDateTime::currentMSecsSinceEpoch() - starttime;
+        }
+        sg04_counts_clear_raw();
+    }
+    //qDebug() << "elapsed time: " << QDateTime::currentMSecsSinceEpoch() - starttime;
     return result;
 }
 
@@ -148,24 +149,24 @@ uint16_t SG04CountProtocol::get_actual_count_rate() {
 }
 
 unsigned int SG04CountProtocol::get_actual_count_rate_cps() {
-	return actual_count_rate * (1000 / SG04_COUNT_INTERVAL_MS);
+    return actual_count_rate * (1000 / SG04_COUNT_INTERVAL_MS);
 }
 
 bool SG04CountProtocol::is_currently_receiving_counts() const {
-	std::unique_lock received_lock{received_counts_mutex};
-	const auto time = std::chrono::system_clock::now() - last_package;
-	return std::chrono::duration_cast<std::chrono::milliseconds>(time) < std::chrono::milliseconds{SG04_COUNT_INTERVAL_MS * 2};
+    std::unique_lock received_lock{received_counts_mutex};
+    const auto time = std::chrono::system_clock::now() - last_package;
+    return std::chrono::duration_cast<std::chrono::milliseconds>(time) < std::chrono::milliseconds{SG04_COUNT_INTERVAL_MS * 2};
 }
 
 bool SG04CountProtocol::is_correct_protocol() {
-	{
-		std::unique_lock received_lock{received_counts_mutex};
-		incoming_data.clear();
-		received_count_packages.clear();
-	}
+    {
+        std::unique_lock received_lock{received_counts_mutex};
+        incoming_data.clear();
+        received_count_packages.clear();
+    }
     if (device->waitReceived(device_protocol_setting.timeout, 4, false)) {
-		std::unique_lock received_lock{received_counts_mutex};
-		return received_count_packages.count();
+        std::unique_lock received_lock{received_counts_mutex};
+        return received_count_packages.count();
     }
     return false;
 }
