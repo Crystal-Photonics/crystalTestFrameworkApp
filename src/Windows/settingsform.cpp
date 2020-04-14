b069d0a3d6a0eebe3760940c0744a2c884f15384
toeger <toeger@anony.mous>
2020-04-14 16:32:53 +0200

    Fixed include path error.

--- src/Windows/settingsform.cpp
+++ src/Windows/settingsform.cpp
@@ -1,8 +1,8 @@
 #include "settingsform.h"
 #include "ui_settingsform.h"
 
+#include "LuaFunctions/lua_functions.h"
 #include "config.h"
-#include "lua_functions.h"
 #include "mainwindow.h"
 #include <QDebug>
 #include <QFileDialog>
@@ -62,9 +62,9 @@ std::vector<std::pair<QLineEdit *, const char *>> SettingsForm::get_config_lines
             {ui->lua_editor_path_text, Globals::lua_editor_path_settings_key},
             {ui->lua_editor_parameters_text, Globals::lua_editor_parameters_settings_key},
             {ui->meta_path_text, Globals::measurement_equipment_meta_data_path_key},
-			{ui->env_var_path, Globals::path_to_environment_variables_key},
-			{ui->luacheck_path, Globals::path_to_luacheck_key},
-			{ui->edit_exceptional_approval, Globals::path_to_excpetional_approval_db_key},
+            {ui->env_var_path, Globals::path_to_environment_variables_key},
+            {ui->luacheck_path, Globals::path_to_luacheck_key},
+            {ui->edit_exceptional_approval, Globals::path_to_excpetional_approval_db_key},
             {ui->favorite_script_path, Globals::favorite_script_file_key},
             {ui->report_history_query, Globals::report_history_query_path},
             {ui->search_path, Globals::search_path_key}};
@@ -180,12 +180,12 @@ void SettingsForm::on_cancel_keySequenceEdit_clear_button_clicked() {
 
 std::vector<SettingsForm::Key_config> SettingsForm::get_key_sequence_config() const {
     return {
-		{ui->confirm_keySequenceEdit, Globals::confirm_key_sequence_key, Globals::default_confirm_key_sequence_key},
-		{ui->skip_keySequenceEdit, Globals::skip_key_sequence_key, Globals::default_skip_key_sequence_key},
-		{ui->cancel_keySequenceEdit, Globals::cancel_key_sequence_key, Globals::default_cancel_key_sequence_key},
+        {ui->confirm_keySequenceEdit, Globals::confirm_key_sequence_key, Globals::default_confirm_key_sequence_key},
+        {ui->skip_keySequenceEdit, Globals::skip_key_sequence_key, Globals::default_skip_key_sequence_key},
+        {ui->cancel_keySequenceEdit, Globals::cancel_key_sequence_key, Globals::default_cancel_key_sequence_key},
     };
 }
 
 void SettingsForm::on_luacheck_path_button_clicked() {
-	request_user_file(ui->luacheck_path, tr("Select luacheck executable"), Globals::path_to_luacheck_key, "*.exe");
+    request_user_file(ui->luacheck_path, tr("Select luacheck executable"), Globals::path_to_luacheck_key, "*.exe");
 }
